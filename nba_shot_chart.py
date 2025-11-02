cde34cf19d9fbcf9f131351a7973e1de0c9ca92b
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-11-01 22:58:19 -0700

    Update nba_shot_chart.py

--- nba_shot_chart.py
+++ nba_shot_chart.py
@@ -297,7 +297,7 @@ def shot_summary(player_id,game_date=game_date, season_long=season_long):
                                        arrowstyle="-|>",
                                        edgecolor='w',
                                        mutation_scale=10,
-                                       mutation_aspect=2))
+                                       mutation_aspect=10))
         # cb.ax.arrow(expected_points,0.5,
         #             actual_points-expected_points,0,
         #             width=0.01,
