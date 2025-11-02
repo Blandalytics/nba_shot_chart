c4c0fe40211ee949de544d558d52a56226d6fdb0
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-11-01 22:49:40 -0700

    Update nba_shot_chart.py

--- nba_shot_chart.py
+++ nba_shot_chart.py
@@ -294,8 +294,8 @@ def shot_summary(player_id,game_date=game_date, season_long=season_long):
     if abs(actual_points-expected_points) >= 0.025:
         cb.ax.annotate("", xytext=(expected_points, 0.5), xy=(actual_points, 0.5),
                        color='g' if actual_points >= expected_points else 'purple',
-                       edgecolor='w',
-                       arrowprops=dict(arrowstyle="->"))
+                       arrowprops=dict(arrowstyle="->",
+                                       edgecolor='w'))
         # cb.ax.arrow(expected_points,0.5,
         #             actual_points-expected_points,0,
         #             width=0.01,
