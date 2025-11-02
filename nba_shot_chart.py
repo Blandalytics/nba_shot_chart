df6b33a836510e5bbc763050566a2e22a021556e
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-11-02 08:53:13 -0800

    Update nba_shot_chart.py

--- nba_shot_chart.py
+++ nba_shot_chart.py
@@ -287,12 +287,12 @@ def shot_summary(player_id,game_date=game_date, season_long=season_long):
     cb.ax.axvline(expected_points,color='k',linewidth=2)
     cb.ax.axvline(actual_points,
                   color='w',
-                  linewidth=3,zorder=9)
+                  linewidth=3.5,zorder=9)
     point_diff = actual_points-expected_points
     scored_more = point_diff >= 0
     cb.ax.axvline(actual_points,
                   color='g' if scored_more else 'purple',
-                  linewidth=1.5,zorder=10)
+                  linewidth=2,zorder=10)
     if abs(point_diff) >= 0.025:
         cb.ax.annotate("", 
                        xytext=(expected_points+0.0025 if scored_more else expected_points-0.0025, 0.5), 
