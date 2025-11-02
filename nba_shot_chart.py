5631a7983b9b623d2ec7265dec03f005c2ca36b2
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-11-01 22:27:28 -0700

    Update nba_shot_chart.py

--- nba_shot_chart.py
+++ nba_shot_chart.py
@@ -211,7 +211,7 @@ def shot_summary(player_id,game_date=game_date, season_long=season_long):
                     marker='o',
                     linewidth=0,
                     edgecolor='w',
-                    s=125,
+                    s=120,
                     legend=False,
                     zorder=12,
                     ax=ax1
