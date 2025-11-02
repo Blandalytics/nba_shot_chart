b9a74fab6e80445200b9287b7c0b987b472725a0
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-11-02 16:00:50 -0700

    Update nba_shot_chart.py

--- nba_shot_chart.py
+++ nba_shot_chart.py
@@ -299,7 +299,7 @@ def shot_summary(player_id,game_date=game_date, season_long=season_long):
                        xy=(np.clip(actual_points+0.0025 if scored_more else actual_points-0.0025,0.69,1.51), 0.5),
                        zorder=9,
                        arrowprops=dict(color='w',
-                                       arrowstyle="-|>, head_width=0.5",
+                                       arrowstyle="-|>, head_width=0.25",
                                        edgecolor='w',
                                        shrinkA=2,shrinkB=0,
                                        linewidth=6))
@@ -308,7 +308,7 @@ def shot_summary(player_id,game_date=game_date, season_long=season_long):
                        xy=(np.clip(actual_points+0.0015 if scored_more else actual_points-0.0015,0.691,1.509), 0.5),
                        zorder=10,
                        arrowprops=dict(color='g' if scored_more else 'purple',
-                                       arrowstyle="-|>, head_width=0.5",
+                                       arrowstyle="-|>, head_width=0.25",
                                        edgecolor=pl_background,linewidth=4))
     ax1.text(0,475,f'xPts/Shot: {expected_points:.2f}            Pts/Shot: {actual_points:.2f}',ha='center',va='center',fontsize=18,fontproperties=prop)
     
