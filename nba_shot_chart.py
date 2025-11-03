2df5989d8411e514a923d4d6bd9f72fa4892f005
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-11-03 00:38:15 -0800

    Update nba_shot_chart.py

--- nba_shot_chart.py
+++ nba_shot_chart.py
@@ -397,7 +397,7 @@ def shot_summary(player_id,game_date=game_date, season_long=season_long):
     ax2.set(xlim=(xlim[0]-0.4,xlim[1]),
            ylim=(0,max_val*1.25))
     ax2.axvline((len(categories)-1)/2,ymax=0.95,color='w',alpha=0.25)
-    ax2.text(4,0,'nba-scoring-chart.streamlit.app',va='bottom',ha='center',fontweight='light', fontsize=10)
+    ax2.text(4.1,0,'nba-scoring-chart.streamlit.app',va='bottom',ha='center',fontweight='light', fontsize=10)
     
     player_name = game_data['PLAYER_NAME'].iloc[0]
     if season_long:
