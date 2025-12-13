7bb614a541827a881526c160dd74d4addad09f81
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-12-13 00:00:07 -0800

    Update nba_shot_chart.py

--- nba_shot_chart.py
+++ nba_shot_chart.py
@@ -26,7 +26,7 @@ pl_line_color = '#293a6b'
 # font_dirs = ["app/static"]  # The path to the custom font file.
 # font_files = fm.findSystemFonts(fontpaths=font_dirs)
 # for font_file in font_files:
-fm.fontManager.addfont('https://github.com/Blandalytics/nba_shot_chart/blob/main/static/Alexandria-VariableFont_wght.ttf')
+fm.fontManager.addfont('/static/Alexandria-VariableFont_wght.ttf')
 st.write(fm.get_font_names())
 
 fpath = os.path.join(os.getcwd(), 'Alexandria/Alexandria-Regular.ttf')
