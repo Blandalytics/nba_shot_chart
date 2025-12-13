2aac77125d4e5f6e870b10aaf5a348516d18c732
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-12-13 00:32:53 -0800

    Update nba_shot_chart.py

--- nba_shot_chart.py
+++ nba_shot_chart.py
@@ -42,7 +42,7 @@ sns.set_theme(
         'legend.facecolor':pl_background,
         'text.color': pl_white
      },
-    # font=font
+    font='Alexandria'
     )
 nba_players = players.get_active_players()
 
